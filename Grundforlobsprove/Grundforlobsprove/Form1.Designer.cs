54822884dd6ad68f0b54831be22201236805c1ab
Eruk347 <kristian_kure@yahoo.dk>
2016-08-26 09:11:22 +0200

    Button and display update
    Added "Cancel" button.
    Added labels to display information that is wanted. Cleared upsome doe
    to make it dynamic and not static

--- Grundforlobsprove/Grundforlobsprove/Form1.Designer.cs
+++ Grundforlobsprove/Grundforlobsprove/Form1.Designer.cs
@@ -39,8 +39,15 @@
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox10 = new System.Windows.Forms.TextBox();
-            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -132,33 +139,107 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 10;
-            this.button1.Text = "button1";
+            this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox10
+            // label2
             // 
-            this.textBox10.Enabled = false;
-            this.textBox10.Location = new System.Drawing.Point(13, 66);
-            this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(87, 20);
-            this.textBox10.TabIndex = 11;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Network address";
             // 
-            // textBox11
+            // label3
             // 
-            this.textBox11.Enabled = false;
-            this.textBox11.Location = new System.Drawing.Point(12, 92);
-            this.textBox11.Name = "textBox11";
-            this.textBox11.Size = new System.Drawing.Size(87, 20);
-            this.textBox11.TabIndex = 12;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Broadcast address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Subnet mask";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Useable addresses";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(117, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(117, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(117, 100);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(117, 117);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "label9";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(347, 262);
-            this.Controls.Add(this.textBox11);
-            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox9);
@@ -190,8 +271,15 @@
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox10;
-        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Grundforlobsprove/Grundforlobsprove/Form1.cs
+++ Grundforlobsprove/Grundforlobsprove/Form1.cs
@@ -12,10 +12,93 @@ namespace Grundforlobsprove
 {
     public partial class Form1 : Form
     {
-        int slash = 0;
+        Int16 slash = 0;
         public Form1()
         {
             InitializeComponent();
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+        }
+
+        private int maskToSlash(int x, int slashNote)
+        {
+            string slashBit = decToBit(x);
+            for (int i = 0; i < slashBit.Length; i++)
+            {
+                if (slashBit.Substring(i, 1) == "1")
+                    slashNote++;
+            }
+            return slashNote;
+        }
+
+        private int bitToDec(string input)
+        {
+            int[] binary = { 128, 64, 32, 16, 8, 4, 2, 1 };
+            int[] deci = new int[8];
+            for (int i = 0; i < input.Length; i++)
+            {
+                deci[i] = Convert.ToInt32(input.Substring(i, 1));
+                deci[i] = deci[i] & binary[i];
+            }
+            return deci.Sum();
+        }
+
+        private string decToBit(int x)
+        {
+            string final = "";
+
+            if (x - 128 >= 0)
+            {
+                final += "1";
+                x -= 128;
+            }
+            else final += "0";
+
+            if (x - 64 >= 0)
+            {
+                final += "1";
+                x -= 64;
+            }
+            else final += "0";
+            if (x - 32 >= 0)
+            {
+                final += "1";
+                x -= 32;
+            }
+            else final += "0";
+            if (x - 16 >= 0)
+            {
+                final += "1";
+                x -= 16;
+            }
+            else final += "0";
+            if (x - 8 >= 0)
+            {
+                final += "1";
+                x -= 8;
+            }
+            else final += "0";
+            if (x - 4 >= 0)
+            {
+                final += "1";
+                x -= 4;
+            }
+            else final += "0";
+            if (x - 2 >= 0)
+            {
+                final += "1";
+                x -= 2;
+            }
+            else final += "0";
+            if (x - 1 >= 0)
+            {
+                final += "1";
+                x -= 1;
+            }
+            else final += "0";
+            return final;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -99,48 +182,24 @@ namespace Grundforlobsprove
         {
             try
             {
-                if (textBox6.Text.Length > 2)
+                if (slash != 1)
                 {
-                    int x = Convert.ToInt32(textBox6.Text);
-                    if (x > 255)
-                        textBox6.Text = "" + 255;
-                    else
+                    slash = 2;
+                    if (textBox6.Text.Length > 2)
                     {
-                        switch (x)
+                        int x = Convert.ToInt32(textBox6.Text);
+                        if (x > 255)
+                            textBox6.Text = "" + 255;
+                        else
                         {
-                            case 254:
-                                textBox9.Text = "15";
-                                break;
-                            case 252:
-                                textBox9.Text = "14";
-                                break;
-                            case 248:
-                                textBox9.Text = "13";
-                                break;
-                            case 240:
-                                textBox9.Text = "12";
-                                break;
-                            case 224:
-                                textBox9.Text = "11";
-                                break;
-                            case 192:
-                                textBox9.Text = "10";
-                                break;
-                            case 128:
-                                textBox9.Text = "9";
-                                break;
-                            default:
-                                textBox9.Text = "16";
-                                break;
+                            textBox9.Text = "" + maskToSlash(x, 8);
+                            textBox7.Text = "0";
+                            textBox8.Text = "0";
                         }
-                        textBox7.Text = "0";
-                        textBox8.Text = "0";
+                        if (textBox6.Text.Length > 2)
+                            textBox7.Focus();
                     }
-                    if (textBox6.Text.Length > 2&& slash==0)
-                        textBox7.Focus();
-                    //textBox9.Enabled = false;
                 }
-
             }
             catch (Exception)
             {
@@ -153,45 +212,20 @@ namespace Grundforlobsprove
         {
             try
             {
-                int x = Convert.ToInt32(textBox7.Text);
-                if (x > 255)
-                    textBox7.Text = "" + 255;
-                else
+                if (slash != 1)
                 {
-                    switch (x)
+                    slash = 2;
+                    int x = Convert.ToInt32(textBox7.Text);
+                    if (x > 255)
+                        textBox7.Text = "" + 255;
+                    else
                     {
-                        case 254:
-                            textBox9.Text = "23";
-                            break;
-                        case 252:
-                            textBox9.Text = "22";
-                            break;
-                        case 248:
-                            textBox9.Text = "21";
-                            break;
-                        case 240:
-                            textBox9.Text = "20";
-                            break;
-                        case 224:
-                            textBox9.Text = "19";
-                            break;
-                        case 192:
-                            textBox9.Text = "18";
-                            break;
-                        case 128:
-                            textBox9.Text = "17";
-                            break;
-                        case 0:
-                            break;
-                        default:
-                            textBox9.Text = "24";
-                            break;
+                        textBox9.Text = "" + maskToSlash(x, 16);
+                        textBox8.Text = "0";
                     }
-                    textBox8.Text = "0";
+                    if (textBox7.Text.Length > 2)
+                        textBox8.Focus();
                 }
-                if (textBox7.Text.Length > 2 && slash == 0)
-                    textBox8.Focus();
-                //textBox9.Enabled = false;
             }
             catch (Exception)
             {
@@ -204,42 +238,19 @@ namespace Grundforlobsprove
         {
             try
             {
-                int x = Convert.ToInt32(textBox8.Text);
-                if (x > 255)
-                    textBox8.Text = "" + 255;
-                else
+                if (slash != 1)
                 {
-                    switch (x)
+                    slash = 2;
+                    int x = Convert.ToInt32(textBox8.Text);
+                    if (x > 255)
+                        textBox8.Text = "" + 255;
+                    else if (Convert.ToInt32(textBox7.Text) > 254)
                     {
-                        case 252:
-                            textBox9.Text = "30";
-                            break;
-                        case 248:
-                            textBox9.Text = "29";
-                            break;
-                        case 240:
-                            textBox9.Text = "28";
-                            break;
-                        case 224:
-                            textBox9.Text = "27";
-                            break;
-                        case 192:
-                            textBox9.Text = "26";
-                            break;
-                        case 128:
-                            textBox9.Text = "25";
-                            break;
-
-                        default:
-                            //textBox9.Text = "24";
-                            break;
+                        textBox9.Text = "" + maskToSlash(x, 24);
                     }
+                    if (textBox8.Text.Length > 2)
+                        button1.Focus();
                 }
-                if (textBox8.Text.Length > 2 && slash == 0)
-                    button1.Focus();
-                //textBox9.Enabled = false;
-
-
             }
             catch (Exception)
             {
@@ -250,25 +261,51 @@ namespace Grundforlobsprove
 
         private void textBox9_TextChanged(object sender, EventArgs e)// slash notation
         {
-            slash = 1;
             try
             {
-                int x = Convert.ToInt32(textBox9.Text);
-                if (x > 30)
-                    textBox9.Text = "" + 30;
-                if(x>24)
+                if (slash != 2)
                 {
-                    textBox6.Text="255";
-                    textBox7.Text = "255";
-                    int sub = x - 24;
-                    double mask = 0 ;
-                    for (int i = 0; i < sub; i++)
+                    slash = 1;
+                    int x = Convert.ToInt32(textBox9.Text);
+                    if (x > 30)
+                        textBox9.Text = "" + 30;
+                    else if (x > 24)
                     {
-                         mask+=Math.Pow(2, 7 - i);
+                        textBox6.Text = "255";
+                        textBox7.Text = "255";
+                        int sub = x - 24;
+                        double mask = 0;
+                        for (int i = 0; i < sub; i++)
+                        {
+                            mask += Math.Pow(2, 7 - i);
+                        }
+                        textBox8.Text = "" + mask;
                     }
-                    textBox8.Text = ""+mask;
+                    else if (x > 16)
+                    {
+                        textBox6.Text = "255";
+                        int sub = x - 16;
+                        double mask = 0;
+                        for (int i = 0; i < sub; i++)
+                        {
+                            mask += Math.Pow(2, 7 - i);
+                        }
+                        textBox7.Text = "" + mask;
+                        textBox8.Text = "0";
+                    }
+                    else
+                    {
+                        int sub = x - 8;
+                        double mask = 0;
+                        for (int i = 0; i < sub; i++)
+                        {
+                            mask += Math.Pow(2, 7 - i);
+                        }
+                        textBox6.Text = "" + mask;
+                        textBox7.Text = "0";
+                        textBox8.Text = "0";
+                    }
                 }
-                
             }
             catch (Exception)
             {
@@ -279,14 +316,74 @@ namespace Grundforlobsprove
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int[] ip = { Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text) };
-            int[] sub = { Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), Convert.ToInt32(textBox7.Text), Convert.ToInt32(textBox8.Text) };
-            int[] ip_sub=new int[4];
-            for (int i = 0; i < 4; i++)
+            try
             {
-                ip_sub[i] = ip[i] & sub[i];
+                slash = 0;
+                int[] ip = { Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text), Convert.ToInt32(textBox4.Text) };
+                int[] sub = { Convert.ToInt32(textBox5.Text), Convert.ToInt32(textBox6.Text), Convert.ToInt32(textBox7.Text), Convert.ToInt32(textBox8.Text) };
+                int[] ip_sub = new int[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    ip_sub[i] = ip[i] & sub[i];
+                }
+                label6.Text = "" + ip_sub[0] + "." + ip_sub[1] + "." + ip_sub[2] + "." + ip_sub[3];
+                label8.Text = "" + textBox5.Text + "." + textBox6.Text + "." + textBox7.Text + "." + textBox8.Text;
+                label9.Text = "";
+
+                int hostBits = 0;
+                string[] subBit = new string[4];
+                subBit[0] = "11111111";
+                for (int i = 1; i < sub.Length; i++)
+                {
+                    subBit[i] = decToBit(sub[i]);
+                    for (int j = 0; j < subBit[i].Length; j++)
+                    {
+                        if (Convert.ToInt32(subBit[i].Substring(j, 1)) == 0)
+                        {
+                            hostBits++;
+                        }
+                    }
+                }
+
+                if(sub[1]<255)
+                {
+                    label7.Text = "" + ip_sub[0] + "."+ip_sub[1]+".255.255";
+                }
+                else if(sub[2]<255)
+                {
+                    label7.Text = "" + ip_sub[0] + "." + ip_sub[1]+"."+ip_sub[2] + ".255";
+                }
+                else
+                    label7.Text = "" + ip_sub[0] + "." + ip_sub[1] + "." + ip_sub[2] + "." + (ip_sub[3] + Math.Pow(2, hostBits) - 1);
+
+                label9.Text = "" + (Math.Pow(2, hostBits) - 2);
             }
-            textBox10.Text = "" + ip_sub[0]+"."+ ip_sub[1] + "." + ip_sub[2] + "." + ip_sub[3];
+            catch (Exception)
+            {
+                //throw;
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            slash = 0;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
         }
     }
 }
